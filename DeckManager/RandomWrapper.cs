bcd6ebdaf4cadea412c3d7ad1bb0f9ab24f82e6c
John Frier <jfrier@gmail.com>
2017-03-03 12:15:54 -0800

    adding 'new' RandomWrapper.cs class

---
+++ DeckManager/RandomWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DeckManager
+{
+    /// <summary>
+    /// Class used to wrap .net's random number generator so it can be mocked out in unit tests.
+    /// </summary>
+    public interface IRandomWrapper
+    {
+        int Next(int minValue, int maxValue);
+    }
+
+    /// <summary>
+    /// Class used to wrap .net's random number generator so it can be mocked out in unit tests.
+    /// </summary>
+    public class RandomWrapper : IRandomWrapper
+    {
+        private Random random;
+
+        public RandomWrapper()
+        {
+            random = new Random();
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return random.Next(minValue, maxValue);
+        }
+    }
+}
